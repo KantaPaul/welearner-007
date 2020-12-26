53ad1578dc597c5712674e1c55cf37305621127d
pobon-xs <33409323+pobon-xs@users.noreply.github.com>
2020-12-27 01:46:28 +0600

    update plugin and demo content

--- plugin/welearner-features/widgets/widgets.php
+++ plugin/welearner-features/widgets/widgets.php
@@ -6,34 +6,34 @@
 if ( function_exists('register_sidebar') ) {
     $footer_widget_one = array(
         'id'            => 'widget_one',
-        'before_widget' => '<div class = "%2$s"><div class = "widget-holder">',
+        'before_widget' => '<div class="%2$s"><div class="widget-holder footer_widget">',
         'after_widget'  => '</div></div>',
-        'before_title'  => '<h3 class  = "widgets-title">',
-        'after_title'   => '</h3>',
+        'before_title'  => '<h4 class="widgets-title">',
+        'after_title'   => '</h4>',
         'name'          => __( 'Footer 1', 'welerner-assistance' ),
     );
     $footer_widget_two = array(
         'id'            => 'widget_two',
-        'before_widget' => '<div class = "%2$s"><div class = "widget-holder">',
+        'before_widget' => '<div class="%2$s"><div class="widget-holder footer_widget">',
         'after_widget'  => '</div></div>',
-        'before_title'  => '<h3 class  = "widgets-title">',
-        'after_title'   => '</h3>',
+        'before_title'  => '<h4 class="widgets-title">',
+        'after_title'   => '</h4>',
         'name'          => __( 'Footer 2', 'welerner-assistance' ),
     );
     $footer_widget_three = array(
         'id'            => 'widget_three',
-        'before_widget' => '<div class = "%2$s"><div class = "widget-holder">',
+        'before_widget' => '<div class = "%2$s"><div class="widget-holder footer_widget">',
         'after_widget'  => '</div></div>',
-        'before_title'  => '<h3 class  = "widgets-title">',
-        'after_title'   => '</h3>',
+        'before_title'  => '<h4 class="widgets-title">',
+        'after_title'   => '</h4>',
         'name'          => __( 'Footer 3', 'welerner-assistance' ),
     );
     $footer_widget_four = array(
         'id'            => 'widget_four',
-        'before_widget' => '<div class = "%2$s"><div class = "widget-holder">',
+        'before_widget' => '<div class="%2$s"><div class="widget-holder footer_widget">',
         'after_widget'  => '</div></div>',
-        'before_title'  => '<h3 class  = "widgets-title">',
-        'after_title'   => '</h3>',
+        'before_title'  => '<h4 class="widgets-title">',
+        'after_title'   => '</h4>',
         'name'          => __( 'Footer 4', 'welerner-assistance' ),
     );
 
